82fe8b9a47783ec4cc594928040a74ae8e028183
pavitr <pavitrchoudhry@gmail.com>
2026-09-21 10:37:30 +0530

    modified:   lecture16/package-lock.json 	modified:   lecture16/package.json 	modified:   lecture16/server.js

--- lecture16/server.js
+++ lecture16/server.js
@@ -2,19 +2,43 @@ const express=require('express');
 const app=express();
 const port=3000;
 
+app.use(morgan());
+
 const logmiddleware=(req,res,next)=>{
     //console.log(`${req.method} ${req.url}`);
-    //next();
     req.name="John Doe";
-    console.log(`Request received from ${req.name}`);
-    res.send("hello from middleware");
+    console.log("Request url:",req.url,"req method",req.method,"Time",new Date().toLocaleString());
+    //res.send("hello from middleware");
+    next();
 })
+
+const apiCheckMiddleware=(req,res,next)=>{
+    if(res.query.API_KEY==="1234");{
+        console.log("authenticator")
+        next();
+    }else{
+        res.send("API invaild")
+    }
 
 app.use(logmiddleware);
+app.use(apiCheckMiddleware);
 
-app.get('/',(req,res)=>{
+app.get('/data',(req,res)=>{
+    console.log("request name:",req.name);
+    console.log("HELLO WORLD");
     res.send('Hello World');
 })
+
+app.get('/data',(req,res)=>{
+    console.log("request name:",req.name);
+    console.log("HELLO WORLD");
+    res.send('Hello World');
+})
+
+app.get("/data",(req,res)=>{
+    console.log("hello world");
+
+}
 
 app.listen(port,()=>{
     console.log(`Server is running on port ${port}`);
